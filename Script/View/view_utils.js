c640d2c8ad1b84f45d0a73ad624ebc3f44360761
nikprosto <gnussum@gmail.com>
2023-03-07 14:58:17 +0300

    feat: add create card function

--- Script/View/view_utils.js
+++ Script/View/view_utils.js
@@ -1,2 +1,31 @@
 //написать функцию прорисовки кнопки, общий вид для всех кнопок в приложении//
 //написать функцию прорисовки модального окна, общий вид для всех модалок в приложении//
+
+
+//создание карточки
+export function createCard(cardParams) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
+    cardContainer.id = crypto.randomUUID()    ;
+
+    const mainImg = document.createElement('img');
+    mainImg.classList.add('card-img-top');
+    mainImg.setAttribute('src', cardParams.url);
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = cardParams.title;
+
+    const cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent= cardParams.description;
+
+    cardBody.append(cardTitle, cardText);
+
+    cardContainer.append(mainImg, cardBody);
+
+    return cardContainer;
+}
