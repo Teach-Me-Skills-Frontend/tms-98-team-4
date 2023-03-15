1f7fe4e66c74f3b5ba426b95d1fa94edf3d24d52
tkachenya41 <tkachenya41@gmail.com>
2023-03-15 19:44:26 +0300

    fix: fixed adaptive header and card list, card text style

--- Script/View/CardList/CardList.js
+++ Script/View/CardList/CardList.js
@@ -1,46 +1,50 @@
 import { createCard } from './CardList_utils.js';
-import { CardAction } from '../../basic_constants.js'
+import { CardAction } from '../../basic_constants.js';
 
 export class CardList {
-    constructor(onCardAction) {
-        this.cardContainer = document.createElement('div');
-        this.cardContainer.setAttribute('id', 'card-container');
-        this.cardContainer.classList.add("container-sm", "d-flex", "flex-wrap", "justify-content-between", "flex-row", "g-3");
+  constructor(onCardAction) {
+    this.cardContainer = document.createElement('div');
+    this.cardContainer.setAttribute('id', 'card-container');
+    this.cardContainer.classList.add(
+      'container-sm','d-flex','flex-wrap','justify-content-evenly','flex-row','g-2'
+    );
 
-        this.cardContainer.addEventListener('click', ({ target }) => {
-            if (Object.values(CardAction).includes(target.dataset.cardAction)) {
-                onCardAction(target.dataset.cardAction, target.src);
-            }
-        })
-    }
+    this.cardContainer.addEventListener('click', ({ target }) => {
+      if (Object.values(CardAction).includes(target.dataset.cardAction)) {
+        onCardAction(target.dataset.cardAction, target.src);
+      }
+    });
+  }
 
-    renderCards = (cards) => {
-        this.cardContainer.innerHTML = '';
-        cards.forEach(picture => {
-            this.cardContainer.append(createCard(picture, { 'data-card-action': CardAction.OpenFull }));
-        })
-    }
+  renderCards = (cards) => {
+    this.cardContainer.innerHTML = '';
+    cards.forEach((picture) => {
+      this.cardContainer.append(
+        createCard(picture, { 'data-card-action': CardAction.OpenFull })
+      );
+    });
+  };
 
-    openPhoto = (src) => {
-        const modalDiv = document.createElement('div');
-        modalDiv.classList.add('modal-div');
-        const root = document.getElementById('root');
-        root.append(modalDiv);
+  openPhoto = (src) => {
+    const modalDiv = document.createElement('div');
+    modalDiv.classList.add('modal-div');
+    const root = document.getElementById('root');
+    root.append(modalDiv);
 
-        const fullPhoto = document.createElement('img');
-        fullPhoto.setAttribute('src', src);
-        modalDiv.append(fullPhoto);
+    const fullPhoto = document.createElement('img');
+    fullPhoto.setAttribute('src', src);
+    modalDiv.append(fullPhoto);
 
-        const closeBtn = document.createElement("i");
-        closeBtn.setAttribute("class", "fas fa-times close-button");
+    const closeBtn = document.createElement('i');
+    closeBtn.setAttribute('class', 'fas fa-times close-button');
 
-        closeBtn.addEventListener('click', (e) => {
-            modalDiv.remove();
-        });
-        modalDiv.addEventListener('click', (e) => {
-            modalDiv.remove();
-        });
+    closeBtn.addEventListener('click', (e) => {
+      modalDiv.remove();
+    });
+    modalDiv.addEventListener('click', (e) => {
+      modalDiv.remove();
+    });
 
-        modalDiv.append(fullPhoto, closeBtn);
-    }
+    modalDiv.append(fullPhoto, closeBtn);
+  };
 }
--- Script/View/CardList/CardList_utils.js
+++ Script/View/CardList/CardList_utils.js
@@ -1,45 +1,45 @@
 function createCardButton() {
-    const cardButton = document.createElement('button');
-    cardButton.classList.add('card-button');
-    cardButton.setAttribute('data-card-action', 'openOptions');
+   const cardButton = document.createElement('button');
+   cardButton.classList.add('card-button');
+   cardButton.setAttribute('data-card-action', 'openOptions');
 
-    return cardButton;
+   return cardButton;
 }
 
 export function createCard(cardParams, CardAction) {
-    const { urls, alt_description, user } = cardParams;
+   const { urls, alt_description, user } = cardParams;
 
-    const card = document.createElement('div');
-    card.classList.add('card', 'shadow');
-    card.id = cardParams.id;
+   const card = document.createElement('div');
+   card.classList.add('card', 'shadow');
+   card.id = cardParams.id;
 
-    const mainImg = document.createElement('img');
-    mainImg.classList.add('card-img-top', 'img-fluid', 'flex-1');
-    mainImg.setAttribute('src', urls.regular);
-    mainImg.setAttribute('data-card-id', cardParams.id);
+   const mainImg = document.createElement('img');
+   mainImg.classList.add('card-img-top', 'img-fluid', 'flex-1');
+   mainImg.setAttribute('src', urls.regular);
+   mainImg.setAttribute('data-card-id', cardParams.id);
 
-    const mainImgContainer = document.createElement('div');
-    mainImgContainer.classList.add('img-container');
-    mainImgContainer.append(mainImg, createCardButton());
+   const mainImgContainer = document.createElement('div');
+   mainImgContainer.classList.add('img-container');
+   mainImgContainer.append(mainImg, createCardButton());
 
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body', 'flex-0');
+   const cardBody = document.createElement('div');
+   cardBody.classList.add('card-body', 'flex-0');
 
-    const author = document.createElement('h5');
-    author.classList.add('card-title');
-    author.textContent = user.name;
+   const author = document.createElement('span');
+   author.classList.add('card-title');
+   author.textContent = user.name;
 
-    const cardText = document.createElement('p');
-    cardText.classList.add('card-text');
-    cardText.textContent = alt_description;
+   const cardText = document.createElement('p');
+   cardText.classList.add('card-text');
+   cardText.textContent = alt_description;
 
-    cardBody.append(author, cardText);
+   cardBody.append(author, cardText);
 
-    card.append(mainImgContainer, cardBody);
+   card.append(mainImgContainer, cardBody);
 
-    for (const key in CardAction) {
-        mainImg.setAttribute(key, CardAction[key]);
-    }
+   for (const key in CardAction) {
+      mainImg.setAttribute(key, CardAction[key]);
+   }
 
-    return card;
+   return card;
 }
